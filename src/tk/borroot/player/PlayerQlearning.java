3eb0fa543e094b0b02b7d9cba608de96dda3f845
Borroot <borroot@live.nl>
2019-11-11 09:13:14 +0100

    Add class for qlearning.

---
+++ src/tk/borroot/player/PlayerQlearning.java
@@ -0,0 +1,11 @@
+package tk.borroot.player;
+
+import tk.borroot.logic.Board;
+
+public class PlayerQlearning extends Player {
+
+    @Override
+    public int move(Board board) {
+        return 0;
+    }
+}
